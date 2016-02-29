836210977505dd0b7c6c5f61d1f7e00a697aa04c
JMGauthierSamares <jm.gauthier@samares-engineering.com>
2016-02-29 21:00:11 +0100

    Xcos menu + EPL licence and copyright

--- Papyrus1.1.3/org.samares.activity2simulink.papyrus.ui/src/org/samares/activity2simulink/papyrus/ui/handlers/ImportXcosModelHandler.java
+++ Papyrus1.1.3/org.samares.activity2simulink.papyrus.ui/src/org/samares/activity2simulink/papyrus/ui/handlers/ImportXcosModelHandler.java
@@ -1,20 +1,21 @@
 /*******************************************************************************
- * Copyright (c) 2016 IBM Corporation and others.
+ * Copyright (c) 2016 Samares-Engineering.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     IBM Corporation - initial API and implementation
+ *    Samares-Engineering - initial API and implementation and/or initial documentation
  *******************************************************************************/
 package org.samares.activity2simulink.papyrus.ui.handlers;
+
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
 import org.eclipse.core.commands.IHandlerListener;
 
-public class ImportSimulinkModelHandler implements IHandler {
+public class ImportXcosModelHandler implements IHandler {
 
 	@Override
 	public void addHandlerListener(IHandlerListener handlerListener) {
