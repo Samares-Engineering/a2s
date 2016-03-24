01bc7717b7f9d07c3e354e5c6dc32edad567c14b
JMGauthierSamares <jm.gauthier@samares-engineering.com>
2016-03-24 12:32:58 +0100

    avancement wizard

--- Papyrus1.1.3/org.samares.activity2simulink.papyrus.ui/src/org/samares/activity2simulink/papyrus/ui/wizard/ImportXCosModelPage.java
+++ Papyrus1.1.3/org.samares.activity2simulink.papyrus.ui/src/org/samares/activity2simulink/papyrus/ui/wizard/ImportXCosModelPage.java
@@ -1,18 +1,25 @@
 package org.samares.activity2simulink.papyrus.ui.wizard;
 
 import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.KeyEvent;
-import org.eclipse.swt.events.KeyListener;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
 public class ImportXCosModelPage extends WizardPage {
 
-	private Text text1;
+	private Text textXcosUserModel;
+	private Text textXcosLibraryModel;
+	private Button browseXcosUserModelButton;
+	private Button browseXcosLibraryButton;
+	
 	private Composite container;
 
 	public ImportXCosModelPage() {
@@ -22,38 +29,82 @@ public class ImportXCosModelPage extends WizardPage {
 		setDescription("Import existing Scilab/XCos model as an activity.");
 	}
 
-
-
 	@Override
 	public void createControl(Composite parent) {
 
-		container = new Composite(parent, SWT.NONE);
+		this.container = new Composite(parent, SWT.NONE);
 		GridLayout layout = new GridLayout();
-		container.setLayout(layout);
+		this.container.setLayout(layout);
 		layout.numColumns = 2;
-		Label label1 = new Label(container, SWT.NONE);
-		label1.setText("Put a value here.");
-		text1 = new Text(container, SWT.BORDER | SWT.SINGLE);
-		text1.setText("");
-		text1.addKeyListener(new KeyListener() {
+
+		GridData gd = new GridData(GridData.FILL_HORIZONTAL);
+		
+		this.browseXcosUserModelButton = new Button(this.container, SWT.PUSH);
+		this.browseXcosUserModelButton.setText("Select user model");
+		
+		this.textXcosUserModel = new Text(container, SWT.BORDER | SWT.SINGLE);
+		this.textXcosUserModel.setLayoutData(gd);
+		this.textXcosUserModel.setEditable(false);
+		
+		this.browseXcosUserModelButton.addSelectionListener(new SelectionListener() {
 
 			@Override
-			public void keyPressed(KeyEvent e) {
-
+			public void widgetSelected(SelectionEvent e) {
+				FileDialog fileDialog = new FileDialog(ImportXCosModelPage.this.container.getShell(), SWT.OPEN);         
+				fileDialog.setText("Import user defined model");
+				String path = fileDialog.open();
+				if (path != null) 
+					textXcosUserModel.setText(path);
 			}
 
 			@Override
-			public void keyReleased(KeyEvent e) {
+			public void widgetDefaultSelected(SelectionEvent e) {
+				// TODO Auto-generated method stub
 
-				if (!text1.getText().isEmpty()) {
+			}
+		});
+		
+		this.browseXcosLibraryButton = new Button(this.container, SWT.PUSH);
+		this.browseXcosLibraryButton.setText("Import from libraries");
+		
+		this.textXcosLibraryModel = new Text(container, SWT.BORDER | SWT.SINGLE);
+		this.textXcosLibraryModel.setLayoutData(gd);
+		this.textXcosLibraryModel.setEditable(false);
+		
+		this.browseXcosLibraryButton.addSelectionListener(new SelectionListener() {
+
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				// TODO Auto-generated method stub
+			}
+
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				// TODO Auto-generated method stub
+			}
+		});
+		
+		
+		this.textXcosUserModel.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent e) {
+				if (!textXcosUserModel.getText().isEmpty()) {
 					setPageComplete(true);
+					browseXcosLibraryButton.setEnabled(false);
 				}
 			}
 		});
-
-		GridData gd = new GridData(GridData.FILL_HORIZONTAL);
-		text1.setLayoutData(gd);
-
+		
+		this.textXcosLibraryModel.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent e) {
+				if (!textXcosUserModel.getText().isEmpty()) {
+					setPageComplete(true);
+					browseXcosUserModelButton.setEnabled(false);
+				}
+			}
+		});
+		
 		// required to avoid an error in the system
 		setControl(container);
 		setPageComplete(false);
@@ -61,8 +112,8 @@ public class ImportXCosModelPage extends WizardPage {
 
 
 
-	public String getText1() {
-		return text1.getText();
+	public String getTextXcosUserModel() {
+		return textXcosUserModel.getText();
 	}
 
 }
--- Papyrus1.1.3/org.samares.activity2simulink.papyrus.ui/src/org/samares/activity2simulink/papyrus/ui/wizard/ImportXcosModelWizard.java
+++ Papyrus1.1.3/org.samares.activity2simulink.papyrus.ui/src/org/samares/activity2simulink/papyrus/ui/wizard/ImportXcosModelWizard.java
@@ -25,7 +25,7 @@ public class ImportXcosModelWizard extends Wizard {
 	@Override
 	public boolean performFinish() {
 		// Print the result to the console
-		System.out.println(one.getText1());
+		System.out.println(one.getTextXcosUserModel());
 
 		return true;
 	}
