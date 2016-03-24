bdbb5f3066d1238433ac432c9559fa5f93a7dc80
JMGauthierSamares <jm.gauthier@samares-engineering.com>
2016-03-24 16:26:09 +0100

    delete warning

--- Papyrus1.1.3/org.samares.activity2simulink.papyrus.ui/src/org/samares/activity2simulink/papyrus/ui/handlers/ImportSimulinkModelHandler.java
+++ Papyrus1.1.3/org.samares.activity2simulink.papyrus.ui/src/org/samares/activity2simulink/papyrus/ui/handlers/ImportSimulinkModelHandler.java
@@ -13,10 +13,6 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
 import org.eclipse.core.commands.IHandlerListener;
-import org.eclipse.jface.wizard.IWizard;
-import org.eclipse.jface.wizard.WizardDialog;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.handlers.HandlerUtil;
 
 public class ImportSimulinkModelHandler implements IHandler {
 
