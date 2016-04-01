3185307bdcc426ae948c3b6ffa8ceef7133e6091
JMGauthierSamares <jm.gauthier@samares-engineering.com>
2016-04-01 11:28:51 +0200

    new project for moka connector

---
+++ Papyrus/org.eclipse.papyrus.activitysimulation.mokaconnector/src/org/eclipse/papyrus/activitysimulation/mokaconnector/engine/OpaqueActionFactory.java
@@ -0,0 +1,23 @@
+package org.eclipse.papyrus.activitysimulation.mokaconnector.engine;
+
+import org.eclipse.papyrus.moka.composites.Semantics.Actions.CompleteActions.CS_ReadExtentActionActivation;
+import org.eclipse.papyrus.moka.composites.Semantics.Loci.LociL3.CS_ExecutionFactory;
+import org.eclipse.papyrus.moka.fuml.Semantics.Loci.LociL1.SemanticVisitor;
+import org.eclipse.uml2.uml.Element;
+import org.eclipse.uml2.uml.OpaqueAction;
+
+public class OpaqueActionFactory extends CS_ExecutionFactory {
+
+	@Override
+	public SemanticVisitor instantiateVisitor(Element element) {
+		SemanticVisitor visitor = null;
+
+		if(element instanceof OpaqueAction){
+			visitor = new OpaqueActionActivation();
+		} else {
+			visitor = super.instantiateVisitor(element);
+		}
+		return visitor;
+	}
+
+}
