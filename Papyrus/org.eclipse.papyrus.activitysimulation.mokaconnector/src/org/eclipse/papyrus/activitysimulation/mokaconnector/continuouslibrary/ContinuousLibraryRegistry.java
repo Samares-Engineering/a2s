bf03d6fca28479a295ed0026f494d08729521336
JMGauthierSamares <jm.gauthier@samares-engineering.com>
2016-04-10 18:01:01 +0200

    Math functions registry

--- Papyrus/org.eclipse.papyrus.activitysimulation.mokaconnector/src/org/eclipse/papyrus/activitysimulation/mokaconnector/continuouslibrary/ContinuousLibraryRegistry.java
+++ Papyrus/org.eclipse.papyrus.activitysimulation.mokaconnector/src/org/eclipse/papyrus/activitysimulation/mokaconnector/continuouslibrary/ContinuousLibraryRegistry.java
@@ -1,6 +1,19 @@
 package org.eclipse.papyrus.activitysimulation.mokaconnector.continuouslibrary;
 
+import org.eclipse.papyrus.activitysimulation.mokaconnector.continuouslibrary.mathfunctions.ACos;
+import org.eclipse.papyrus.activitysimulation.mokaconnector.continuouslibrary.mathfunctions.ASin;
+import org.eclipse.papyrus.activitysimulation.mokaconnector.continuouslibrary.mathfunctions.ATan;
+import org.eclipse.papyrus.activitysimulation.mokaconnector.continuouslibrary.mathfunctions.Cos;
+import org.eclipse.papyrus.activitysimulation.mokaconnector.continuouslibrary.mathfunctions.Cosh;
+import org.eclipse.papyrus.activitysimulation.mokaconnector.continuouslibrary.mathfunctions.Exp;
+import org.eclipse.papyrus.activitysimulation.mokaconnector.continuouslibrary.mathfunctions.Log;
+import org.eclipse.papyrus.activitysimulation.mokaconnector.continuouslibrary.mathfunctions.Log10;
+import org.eclipse.papyrus.activitysimulation.mokaconnector.continuouslibrary.mathfunctions.Power;
 import org.eclipse.papyrus.activitysimulation.mokaconnector.continuouslibrary.mathfunctions.Sin;
+import org.eclipse.papyrus.activitysimulation.mokaconnector.continuouslibrary.mathfunctions.Sinh;
+import org.eclipse.papyrus.activitysimulation.mokaconnector.continuouslibrary.mathfunctions.Sqrt;
+import org.eclipse.papyrus.activitysimulation.mokaconnector.continuouslibrary.mathfunctions.Tan;
+import org.eclipse.papyrus.activitysimulation.mokaconnector.continuouslibrary.mathfunctions.Tanh;
 import org.eclipse.papyrus.moka.fuml.Semantics.Loci.LociL1.Locus;
 import org.eclipse.papyrus.moka.fuml.registry.AbstractOpaqueBehaviorExecutionRegistry;
 
@@ -16,10 +29,49 @@ public class ContinuousLibraryRegistry extends AbstractOpaqueBehaviorExecutionRe
 		try {
 			// ////////////////////
 			// Math functions
-			
+
+			// ACos
+			this.registerOpaqueBehaviorExecution(new ACos(), "ContinuousLibrary::MathFunctions::ACos");
+
+			// ASin
+			this.registerOpaqueBehaviorExecution(new ASin(), "ContinuousLibrary::MathFunctions::ASin");
+
+			// ATan
+			this.registerOpaqueBehaviorExecution(new ATan(), "ContinuousLibrary::MathFunctions::ATan");
+
+			// Cos
+			this.registerOpaqueBehaviorExecution(new Cos(), "ContinuousLibrary::MathFunctions::Cos");
+
+			// Cosh
+			this.registerOpaqueBehaviorExecution(new Cosh(), "ContinuousLibrary::MathFunctions::Cosh");
+
+			// Exp
+			this.registerOpaqueBehaviorExecution(new Exp(), "ContinuousLibrary::MathFunctions::Exp");
+
+			// Log
+			this.registerOpaqueBehaviorExecution(new Log(), "ContinuousLibrary::MathFunctions::Log");
+
+			// Log10
+			this.registerOpaqueBehaviorExecution(new Log10(), "ContinuousLibrary::MathFunctions::Log10");
+
+			// Power
+			this.registerOpaqueBehaviorExecution(new Power(), "ContinuousLibrary::MathFunctions::Power");
+
 			// Sin
 			this.registerOpaqueBehaviorExecution(new Sin(), "ContinuousLibrary::MathFunctions::Sin");
-			
+
+			// Sinh
+			this.registerOpaqueBehaviorExecution(new Sinh(), "ContinuousLibrary::MathFunctions::Sinh");
+
+			// Sqrt
+			this.registerOpaqueBehaviorExecution(new Sqrt(), "ContinuousLibrary::MathFunctions::Sqrt");
+
+			// Tan
+			this.registerOpaqueBehaviorExecution(new Tan(), "ContinuousLibrary::MathFunctions::Tan");
+
+			// Tanh
+			this.registerOpaqueBehaviorExecution(new Tanh(), "ContinuousLibrary::MathFunctions::Tanh");
+
 		} catch (Exception e) {
 			org.eclipse.papyrus.infra.core.Activator.log.error(e);
 		}
